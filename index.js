cddab01d972bd11da70c07b71198696a3a128391
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-06-29 13:19:42 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // Getting all combinations
+  let result = array.reduce( (acc, v, i) =>
+  acc.concat(array.slice(i+1).map( w => v + w )),
+[]);
+  //console.log(result)
+
+  // Checking if target in combinations
+  if (result.includes(target)){
+    return true
+  } else {
+    return false
+  }
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,11 +22,11 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+*/ //commented in code
 
 /*
   Add written explanation of your solution here
-*/
+*/ //commented in code 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
